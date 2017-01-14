2d46fa38035dca2c0fa204d6c2a76498e7c110ba
martsime <simensen94@gmail.com>
2017-01-14 15:51:24 +0100

    Update requirements
    - Add django-bootstrap3 to requirements
    - Update setup script and test data
    - Add .editorconfig

--- DSSCDB/settings.py
+++ DSSCDB/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'dye',
     'usermanagement',
     'sorl.thumbnail',           # Image thumbnails and caching
-    'extended_choices'          # Better enumeration support for choices
+    'extended_choices',         # Better enumeration support for choices
+    'bootstrap3',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -124,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    '/var/www/static/',
+]
