1c0741d40fc741fa39eea21d402b60fe84ffb12a
martsime <simensen94@gmail.com>
2017-01-21 15:17:50 +0100

    Update page for listing performances

--- DSSCDB/urls.py
+++ DSSCDB/urls.py
@@ -1,12 +1,20 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from DSSCDB import views
-from usermanagement import views as usermanagement_views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles import views as static_views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^user/', include('usermanagement.urls', namespace='user')),
     url(r'^hub/', include('dye.urls', namespace='dye')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
     url(r'^', include('django.contrib.flatpages.urls', namespace='flatpages')),
 ]
