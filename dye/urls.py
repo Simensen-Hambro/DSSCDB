0849b5f2acd9faf2e0e0b679934d80a854bbb2ea
Carl Johan Hambro <carljohan.hambro@gmail.com>
2017-01-22 20:32:49 +0100

    Minor fixes

--- dye/urls.py
+++ dye/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^performance-list/keyword-search/$', views.performance_keyword_search, name='performance-keyword-search'),
     url(r'^performance-list/range-search/$', views.performance_range_search, name='performance-range-search'),
     url(r'^performance-detail/(?P<short_id>[0-9a-f]{8})/$', views.performance_details, name='performance-detail'),
-    url(r'^contributions/(?P<short_id>[0-9a-f]{8})/$', views.single_contribution_evaluation, name='single-evaluation'),
+    url(r'^contributions/(?P<short_id>[0-9a-f]{8})/$', views.contribution_performances, name='single-evaluation'),
     url(r'^contributions/$', views.contributions_evaluation_overview, name='evaluate-contributions'),
     url(r'^my-contributions/$', views.my_contributions, name='my-contributions'),
 ]
--- dye/validators.py
+++ dye/validators.py
@@ -1,16 +1,22 @@
 from rdkit.Chem import MolFromInchi, MolFromSmiles
+from django.core.exceptions import ValidationError
 
 
 def validate_smiles(data):
     molecule = MolFromSmiles(data)
-    if molecule:
-        return True
-    else:
-        return False
+    if not molecule:
+        raise ValidationError('{} is not a valid SMILES'.format(data))
+
 
 def validate_inchi(data):
     molecule = MolFromInchi(data)
-    if molecule:
-        return True
-    else:
-        return False
+    if not molecule:
+        raise ValidationError('{} is not a valid INCHI'.format(data))
+
+
+def not_negative(data):
+    return data > 0
+
+
+def between_0_and_1(data):
+    return 0 < data < 1
