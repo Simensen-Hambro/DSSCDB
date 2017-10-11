fcafd5011289b590fa02bce6cb61c3f2ee2309a8
Carl Johan Hambro <carljohan.hambro@gmail.com>
2017-10-11 21:50:40 +0200

    Fix email script

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -15,7 +15,7 @@ fi
 source activate rdkitenv
 
 echo "##### STARTING UWSGI CRON AS DAEMON #####"
-uwsgi -d /dev/null --module=DSSCDB.cron_wsgi:application --master --pidfile=/tmp/project-cron.pid --logto /dev/stdout --http=0.0.0.0:8001 --processes=5 --harakiri=300 --max-requests=5000 --vacuum
+uwsgi -d /dev/stderr --module=DSSCDB.cron_wsgi:application --pidfile=/tmp/project-cron.pid --master --logto /dev/stdout --http=0.0.0.0:8001 --processes=1 --harakiri=300 --enable-threads --vacuum
 
 echo "##### UWSGI MAIN #####"
 uwsgi --module=DSSCDB.wsgi:application --master --pidfile=/tmp/project-master.pid --logto /dev/stderr --http=0.0.0.0:8000 --processes=5 --harakiri=300 --max-requests=5000 --vacuum
