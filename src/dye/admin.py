42f6f8c42276e2e040fa192f100e5a4fed7dd84f
Carl Johan Hambro <carljohan.hambro@gmail.com>
2018-02-06 00:06:49 +0100

    Add search field for inchi on admin panel

--- src/dye/admin.py
+++ src/dye/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from .models import Article, Molecule, Spectrum, Performance, Spreadsheet, Contribution, AtomicContribution
 
+
+@admin.register(Molecule)
+class EventAdmin(admin.ModelAdmin):
+    search_fields = ('inchi',)
+
+
 admin.site.register(Article)
-admin.site.register(Molecule)
 admin.site.register(Spectrum)
 admin.site.register(Performance)
 admin.site.register(Spreadsheet)
