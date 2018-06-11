6631110c1bb477d45eab9c15324826958cd61ed6
Carl Johan Hambro <carljohan.hambro@gmail.com>
2018-06-11 23:47:23 +0200

    Add Pybel as backoff package when creating 2D SVGs for molecules

--- src/dye/signals.py
+++ src/dye/signals.py
@@ -28,26 +28,40 @@ def generate_molecule_data(sender, instance, signal, created, **kwargs):
 
         image_name = uuid4().hex[:10]
         smiles = instance.smiles
-        molecule = Chem.MolFromSmiles(smiles)
-        AllChem.Compute2DCoords(molecule)
+
+
+        rdkit_molecule = Chem.MolFromSmiles(smiles)
+        pybel_object = pybel.readstring('smiles', instance.smiles)
+
+        if rdkit_molecule:
+            AllChem.Compute2DCoords(rdkit_molecule)
+
         file_name = 'molecules/' + '{}.{}'.format(image_name, format)
         image_url = settings.MEDIA_ROOT + '/' +  file_name
         # 2D representation by RDKIT
         try:
             os.makedirs(os.path.dirname(image_url), exist_ok=True)
-            Draw.MolToFile(molecule, image_url, size=(400, 400), type=format)
+            if rdkit_molecule:
+                Draw.MolToFile(rdkit_molecule, image_url, size=(400, 400), type=format)
+            else:
+                with open(image_url, 'w+') as fi:
+                    svg = pybel_object._repr_svg_()
+                    fi.writelines(svg)
+
             instance.image = file_name
             logger.info('INFO: Image {} generated for SMILES: {}'.format(image_url, smiles))
         except Exception as e:
             logger.error('ERROR: Error message: {}, for SMILES: {}'.format(e, smiles))
+
         # SDF (3D-coordinates) generations
         sdf_string = generate_coordinates_babel(instance.smiles)
         if sdf_string == '':
             logger.error('ERROR: Failed to generate SDF for {}'.format(instance.smiles))
 
+        # Create 3D-representation (SDF)
         instance.representation_3d = sdf_string
 
-        pybel_object = pybel.readstring('smiles', instance.smiles)
+        # Assign fingerprint
         instance.fingerprint = str(pybel_object.calcfp(fptype='fp2'))
 
         instance.save()
